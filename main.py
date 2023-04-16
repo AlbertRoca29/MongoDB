c191b27b6cca5d27fb415ecc064f5c425356437d
mireiamajo <128802650+mireiamajo@users.noreply.github.com>
2023-04-16 20:47:38 +0200

    Millores coding style + comentaris
    Petits canvis sense efectes sobre el funcionament del codi. Tan sols he tocat alguns aspectes del coding style i he fet més explícits els comentaris que he considerat que requerien més informació.

--- main.py
+++ main.py
@@ -1,51 +1,49 @@
 import pandas as pd
 from pymongo import MongoClient
-import json
 
 # Lectura de les dades a l'Excel
 dades_cp = pd.read_excel('Dades.xlsx', sheet_name='Colleccions-Publicacions')
 dades_p = pd.read_excel('Dades.xlsx', sheet_name='Personatges')
 dades_a = pd.read_excel('Dades.xlsx', sheet_name='Artistes')
 
-# 4 coleccions
-dades_editorials = dades_cp[['NomEditorial','resposable','adreca','pais']].drop_duplicates().reset_index(drop=True).to_dict('records')
+
+# Emmagatzematge de les dades en cadascuna de les 4 col·leccions
+dades_editorials = dades_cp[['NomEditorial','resposable','adreca','pais']
+                            ].drop_duplicates().reset_index(drop=True).to_dict('records')
 
 dades_artistes = dades_a.to_dict('records')
 
-dades_colleccions = dades_cp[['NomColleccio','genere','idioma','any_inici',
-                              'any_fi','tancada']].drop_duplicates().reset_index(drop=True).to_dict('records')
+dades_colleccions = dades_cp[['NomColleccio','genere','idioma','any_inici','any_fi','tancada']
+                             ].drop_duplicates().reset_index(drop=True).to_dict('records')
 
-dades_publicacions = dades_cp[['ISBN','titol','stock','autor','preu', 'num_pagines', 'guionistes', 'dibuixants', 'NomColleccio', 'NomEditorial']].to_dict('records')
+dades_publicacions = dades_cp[['ISBN','titol','stock','autor','preu','num_pagines',
+                               'guionistes','dibuixants','NomColleccio','NomEditorial']
+                              ].to_dict('records')
 
-
-
-# Afegir personatges a publicacions
-
+# Addicio de les dades dels personatges a publicacions
+# A traves de l'ISBN, relacionem cada personatge amb el(s) llibre(s) on apareix
 personatges = dades_p.to_dict('records')
 
 for publicacio in dades_publicacions:
     isbn = publicacio['ISBN']
-    personatges_publicacio = [{k: v for k, v in char.items() if k != 'isbn'} for char in personatges if char['isbn'] == isbn]
+    personatges_publicacio = [{k: v for k, v in char.items() if k != 'isbn'}
+                              for char in personatges if char['isbn'] == isbn]
     if (len(personatges_publicacio)) :
         publicacio['personatges'] = personatges_publicacio
         
-        
-        
-# Llistes
+# Format de les llistes
 for dades in [dades_publicacions,dades_colleccions]:
     for doc in dades:
         for key, value in doc.items():
             if isinstance(value, str) and '[' in value and ']' in value:
                 doc[key] = value.strip('][').split(', ')
 
-# filtrar any_fi
+# Si una col·leccio no esta tancada, el seu document no tindra el camp 'any_fi'
 for i,doc in enumerate(dades_colleccions):
     if not doc['tancada']:
         del dades_colleccions[i]['any_fi']
     else:
         dades_colleccions[i]['any_fi'] = int(dades_colleccions[i]['any_fi'])
-
-
 
 # En execucio remota
 Host = 'localhost' 
